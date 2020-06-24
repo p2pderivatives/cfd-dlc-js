55050386e05006a0e5ca7a486c40b30521fbc1d6
Simon <Simonrico0 7@gmail.com>
2020-06-24 12:25:48 +0900

    update cfd-dlc to latest, cfd-js v0.1.9, wallet-for-testing-js v0.0.9

--- integration_tests/__tests__/dlctests.spec.ts
+++ integration_tests/__tests__/dlctests.spec.ts
@@ -1,11 +1,11 @@
-import * as CfdUtils from "../../wrap_js/cfd_utils";
 import * as CfdDlcUtils from "../../wrap_js/cfddlc_utils";
-import { DlcWalletHelper } from "./wallet_helper";
+import * as CfdUtils from "../../wrap_js/cfd_utils";
 import { DlcTestHelper } from "./test_helper";
+import { DlcWalletHelper } from "./wallet_helper";
 
 const SyncTimeout = 5000;
 
-jest.setTimeout(30000);
+jest.setTimeout(50000);
 const walletHelper = new DlcWalletHelper();
 
 beforeAll(async () => {
